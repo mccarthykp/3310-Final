b4a16b1e5d8f18b8ed2f0f7f658d7d1984f072d4
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2022-07-13 17:41:23 -0400

    Completed Ch1 & Ch2

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+function ordinalSuffix(num: any) {
+
+  if (num > 3 && num < 21) {
+    return num + 'th';
+  }
+
+  switch (num % 10) {
+    case 1:
+      return num + 'st';
+    case 2:
+      return num + 'nd';
+    case 3: 
+      return num + 'rd';
+    default:
+      return num + 'th';
+  }
+}
+
+module.exports.ordinalSuffix = ordinalSuffix;
---
+++ tests/index.test.js
@@ -0,0 +1,33 @@
+const os = require('../src/index.js');
+
+test('Test ordinalSuffix for 1', () => {
+  expect(os(1)).toBe('1st');
+});
+
+test('Test ordinalSuffix for 2', () => {
+  expect(os(2)).toBe('2nd');
+});
+
+test('Test ordinalSuffix for 3', () => {
+  expect(os(3)).toBe('3rd');
+});
+
+test('Test ordinalSuffix for 4', () => {
+  expect(os(4)).toBe('4th');
+});
+
+test('Test ordinalSuffix for 11', () => {
+  expect(os(11)).toBe('11th');
+});
+
+test('Test ordinalSuffix for 502', () => {
+  expect(os(502)).toBe('502nd');
+});
+
+test('Test ordinalSuffix for 1001', () => {
+  expect(os(1001)).toBe('1001st');
+});
+
+test('Test ordinalSuffix for 433', () => {
+  expect(os(433)).toBe('433rd');
+});
